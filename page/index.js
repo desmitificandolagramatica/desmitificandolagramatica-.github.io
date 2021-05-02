97c3eaada692c2d3ca990076df8964351af74b0c
AFelipeCR <AFelipeCR>
2021-05-02 13:37:56 -0500

    a

--- page/index.js
+++
@@ -1,7 +0,0 @@
-const guidesRef = document.getElementById('guides')
-const logoRef = document.getElementById('logo')
-
-function showGuides(){
-	guidesRef.classList.remove('guide-block-none')
-	logoRef.classList.add('guide-block-none')
-}
